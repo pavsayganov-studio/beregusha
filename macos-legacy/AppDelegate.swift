17079bc2f76ab0d9a33c6c285517c210107d0871
pavsayganov-studio <pav.sayganov@gmail.com>
2026-05-14 20:22:34 +0000

    fix: repair corrupted files from terminal paste

--- macos-legacy/AppDelegate.swift
+++ macos-legacy/AppDelegate.swift
@@ -1,11 +1,31 @@
 import Cocoa
+
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusItem: NSStatusItem!
+    var popover: NSPopover!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         if let button = statusItem.button {
-            button.title = "VPN (10.10)"
-            // Здесь позже добавим вызов твоего нового минималистичного UI
+            button.title = "✈️"
+            button.action = #selector(togglePopover(_:))
+            button.target = self
+        }
+        
+        popover = NSPopover()
+        popover.contentViewController = ViewController()
+        popover.behavior = .transient
+        popover.appearance = NSAppearance(named: .vibrantDark)
+    }
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if popover.isShown {
+            popover.performClose(sender)
+        } else {
+            if let button = statusItem.button {
+                NSApp.activate(ignoringOtherApps: true)
+                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            }
         }
     }
 }
